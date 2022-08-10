cc4bc7ef49c5ed6461b607e0a6ba93f220bb72e0
Ovidiu Popa <ovidiu.popa@armory.io>
2022-08-10 19:14:35 +0300

    feat: Build and expose deck
    SPLAT-302

---
+++ Docker/docker/run-apache2.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+set -e
+# Set any missing env variables used to configure deck
+
+_DECK_HOST=$DECK_HOST
+_DECK_PORT=$DECK_PORT
+_API_HOST=$API_HOST
+_DECK_CERT_PATH=$DECK_CERT
+_DECK_KEY_PATH=$DECK_KEY
+_PASSPHRASE=$PASSPHRASE
+
+if [ -z "$_DECK_HOST" ]; then
+    _DECK_HOST=0.0.0.0
+fi
+
+if [ -z "$_DECK_PORT" ]; then
+    _DECK_PORT=9000
+fi
+
+if [ -z "$_API_HOST" ]; then
+    _API_HOST=http://localhost:8084
+fi
+
+if [ -z "$_DECK_CERT_PATH" ]; then
+	# SSL not enabled
+	cp /opt/deck/bin/spinnaker.conf.gen spinnaker.conf
+else
+	cp bin/spinnaker.conf.ssl spinnaker.conf
+	sed -ie 's|{%DECK_CERT_PATH%}|'$_DECK_CERT_PATH'|g' spinnaker.conf
+	sed -ie 's|{%DECK_KEY_PATH%}|'$_DECK_KEY_PATH'|g' spinnaker.conf
+fi
+
+# Generate spinnaker.conf site & enable it
+sed -ie 's|{%DECK_HOST%}|'$_DECK_HOST'|g' spinnaker.conf
+sed -ie 's|{%DECK_PORT%}|'$_DECK_PORT'|g' spinnaker.conf
+sed -ie 's|{%API_HOST%}|'$_API_HOST'|g' spinnaker.conf
+
+mv spinnaker.conf  /etc/apache2/
+
+cp /opt/deck/bin/httpd.conf.gen /etc/apache2/httpd.conf
+
+# Create a passphrase file to inject the SSL passphrase into apache's startup
+cp /opt/deck/bin/passphrase.gen passphrase
+
+sed -ie "s|{%PASSPHRASE%}|$_PASSPHRASE|g" passphrase
+
+# Clear password from env vars
+_PASSPHRASE=""
+PASSPHRASE=""
+
+chmod +x passphrase
+mv passphrase /etc/apache2/passphrase
+
+if [ -e /opt/spinnaker/config/settings.js ]; then
+	cp /opt/spinnaker/config/settings.js /opt/deck/html/settings.js
+fi
+
+if [ -e /opt/spinnaker/config/settings-local.js ]; then
+	cp /opt/spinnaker/config/settings-local.js /opt/deck/html/settings-local.js
+fi
+
+if [ -e /opt/spinnaker/config/plugin-manifest.json ];
+then
+  cp /opt/spinnaker/config/plugin-manifest.json /opt/deck/html/plugin-manifest.json
+else
+  echo '[]' > /opt/deck/html/plugin-manifest.json
+fi
+
+LD_LIBRARY_PATH=/usr/lib/apache2 apachectl -DFOREGROUND
---
+++ Docker/docker/setup-apache2.sh
@@ -0,0 +1,10 @@
+chown -R www-data:www-data /etc/apache2
+
+mkdir -p /var/lib/apache2
+chown -R www-data:www-data /var/lib/apache2
+
+mkdir -p /var/run/apache2
+chown -R www-data:www-data /var/run/apache2
+
+mkdir -p /var/log/apache2
+chown -R www-data:www-data /var/log/apache2
