4e0f22dbb76efbaec164200e415f45eb1e44d2d4
Carlos Samuel Simental Castro <cssimental@Carloss-MacBook-Pro.local>
2022-09-07 15:21:12 -0600

    changed start messages

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,15 +1,23 @@
 /usr/bin/supervisord  -n -c /etc/supervisor/conf.d/supervisord.conf &
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:80/api/v1/notifications/metadata)" != "200" ]]
 do 
-	clear
 	echo ''
-    echo -ne ' WAIT for Spinnaker to be ready .    '\\r
-	sleep 2
-	echo -ne ' WAIT for Spinnaker to be ready . .'\\r 
-	sleep 2
-	echo -ne ' WAIT for Spinnaker to be ready . . .'\\r
+    echo ' WAIT for Spinnaker to be ready .    '
+    sleep 2
+    echo ''
+    echo ' WAIT for Spinnaker to be ready . .  '
+    sleep 2
+    echo ''
+    echo ' WAIT for Spinnaker to be ready . . .'
 	sleep 2
 done
-echo '                                       '
-echo 'Quick-Spin is ready to go!'
+echo '    +---------------------------------+'
+echo '    |                                 |'
+echo '    | Quick-Spin is ready to go!      |'
+echo '    |                                 |'
+echo '    | Go to: http://localhost:9000    |'
+echo '    |                                 |'
+echo '    | To stop Quick spin press ctrl+c |'
+echo '    |                                 |'
+echo '    +---------------------------------+'
 wait
