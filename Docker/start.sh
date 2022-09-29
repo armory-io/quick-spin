aa178908e993483e40b20d1560e29eb1ea7f871b
Mark Rampton <mark.rampton@armory.io>
2022-09-29 10:54:44 -0700

    fix(assorted): small issues and improvements

--- Docker/start.sh
+++ Docker/start.sh
@@ -2,22 +2,22 @@
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:80/api/v1/notifications/metadata)" != "200" ]]
 do 
 	echo ''
-    echo ' Waiting for Quick-Spin to become ready .    '
+    echo ' Waiting for Quick Spin to become ready .    '
     sleep 2
     echo ''
-    echo ' Waiting for Quick-Spin to become ready . .  '
+    echo ' Waiting for Quick Spin to become ready . .  '
     sleep 2
     echo ''
-    echo ' Waiting for Quick-Spin to become ready . . .'
+    echo ' Waiting for Quick Spin to become ready . . .'
 	sleep 2
 done
 echo '    +---------------------------------+'
 echo '    |                                 |'
-echo '    | Quick-Spin is ready to go!      |'
+echo '    | Quick Spin is ready to go!      |'
 echo '    |                                 |'
 echo '    | Go to: http://localhost:9000    |'
 echo '    |                                 |'
-echo '    | To stop Quick-Spin press ctrl+c |'
+echo '    | To stop Quick Spin press ctrl+c |'
 echo '    |                                 |'
 echo '    +---------------------------------+'
 wait
